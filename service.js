cef050cf841c957e797d22a966f44092f2c2d9d9
webbag <139613375+webbag1@users.noreply.github.com>
2024-07-29 08:25:11 +0530

    Add files via upload

--- service.js
+++ service.js
@@ -29,25 +29,25 @@ let img_list_seo = [
   {img_herf: './img/seo/seo3.jpg'}
 ]
 let count_num_seo = 0;
-function update_img(count_num_seo) {
+function update_img_seo(count_num_seo) {
     img1_seo.setAttribute('src', `${img_list_seo[count_num_seo].img_herf}`);
 }
 chevron_right_seo.addEventListener('click', () => {
   if(count_num_seo == img_list_seo.length-1){
     count_num_seo = 0;
-    update_img(count_num_seo);
+    update_img_seo(count_num_seo);
   }else{
     count_num_seo++;
-    update_img(count_num_seo);
+    update_img_seo(count_num_seo);
   }
 })
 chevron_left_seo.addEventListener('click', () => {
   if(count_num_seo == 0){
     count_num_seo = img_list_seo.length-1;
-    update_img(count_num_seo);
+    update_img_seo(count_num_seo);
   }else{
     count_num_seo--;
-    update_img(count_num_seo);
+    update_img_seo(count_num_seo);
   }
 })
 
@@ -94,25 +94,25 @@ let img_list_web = [
   {img_herf: './img/web/web3.jpg'}
 ]
 let count_num_web = 0;
-function update_img(count_num_web) {
+function update_img_web(count_num_web) {
     img1_web.setAttribute('src', `${img_list_web[count_num_web].img_herf}`);
 }
 chevron_right_web.addEventListener('click', () => {
   if(count_num_web == img_list_web.length-1){
     count_num_web = 0;
-    update_img(count_num_web);
+    update_img_web(count_num_web);
   }else{
     count_num_web++;
-    update_img(count_num_web);
+    update_img_web(count_num_web);
   }
 })
 chevron_left_web.addEventListener('click', () => {
   if(count_num_web == 0){
     count_num_web = img_list_web.length-1;
-    update_img(count_num_web);
+    update_img_web(count_num_web);
   }else{
     count_num_web--;
-    update_img(count_num_web);
+    update_img_web(count_num_web);
   }
 })
 
@@ -126,24 +126,24 @@ let img_list_meta = [
   {img_herf: './img/meta/meta3.jpg'}
 ]
 let count_num_meta = 0;
-function update_img(count_num_meta) {
+function update_img_meta(count_num_meta) {
     img1_meta.setAttribute('src', `${img_list_meta[count_num_meta].img_herf}`);
 }
 chevron_right_meta.addEventListener('click', () => {
   if(count_num_meta == img_list_meta.length-1){
     count_num_meta = 0;
-    update_img(count_num_meta);
+    update_img_meta(count_num_meta);
   }else{
     count_num_meta++;
-    update_img(count_num_meta);
+    update_img_meta(count_num_meta);
   }
 })
 chevron_left_meta.addEventListener('click', () => {
   if(count_num_meta == 0){
     count_num_meta = img_list_meta.length-1;
-    update_img(count_num_meta);
+    update_img_meta(count_num_meta);
   }else{
     count_num_meta--;
-    update_img(count_num_meta);
+    update_img_meta(count_num_meta);
   }
 })
